1a5704695a91b995700351da228621e9fe84b856
Henrique <hfr@gmx.co.uk>
2021-01-27 23:09:03 -0300

    jump start

--- t2-2020.1/threejs/anim_2.js
+++ t2-2020.1/threejs/anim_2.js
@@ -1,2 +1,23 @@
+function JumpAnimation() {}
+
+Object.assign( WaveAnimation.prototype, {
+
+    init: function() {
 
 
+        
+
+    },
+    animate: function(time) {
+        window.requestAnimationFrame(this.animate.bind(this));
+        TWEEN.update(time);
+    },
+    run: function() {
+        this.init();
+        this.animate(0);
+    }
+
+
+
+
+});
